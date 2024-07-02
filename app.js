02db54047a226a637af9d52a94832dbcf6a90603
izabela da silva neves <izabeladasilva10@gmail.com>
2024-07-02 19:18:44 -0300

    update: refactoring functions

--- app.js
+++ app.js
@@ -2,29 +2,38 @@
 
 const formNameBook = document.querySelector("#formName")
 const inputNameBook = document.querySelector("#search")
+const btnInputSearch = document.querySelector("#btnSearch")
+const displayBookArea = document.querySelector(".books")
+let dataBooks = null
+let books = null
 
-formNameBook.addEventListener("submit", async (e) => {
+const getDataOfApi = async () => {
+  if (!dataBooks) {
+    try {
+      const response = await axios.get(
+        `https://stephen-king-api.onrender.com/api/books`
+      )
+      dataBooks = response.data.data
+      return dataBooks
+    } catch (error) {
+      console.log("error: ", error)
+    }
+  }
+  return dataBooks
+}
+
+btnInputSearch.addEventListener("click", async (e) => {
   e.preventDefault()
-  const nameOfTheBook = inputNameBook.value.toLowerCase()
+  let nameOfTheBookInput = inputNameBook.value.toLowerCase()
   try {
-    let result = await getNameOfTheBook(nameOfTheBook)
+    let result = await getDataOfApi()
+    books = findBook(nameOfTheBookInput, result)
+    displayBookArea.innerHTML = ""
+    renderSearch(books)
   } catch (error) {
     console.log("error: ", error)
   }
 })
-
-const getNameOfTheBook = async (name) => {
-  try {
-    const response = await axios.get(
-      `https://stephen-king-api.onrender.com/api/books`
-    )
-    const dataBooks = response.data.data
-    const resultSearch = findBook(name, dataBooks)
-    return resultSearch
-  } catch (error) {
-    console.log("error: ", error)
-  }
-}
 
 const findBook = (name, dataBook) => {
   const book = dataBook.filter((data) =>
@@ -33,33 +42,35 @@ const findBook = (name, dataBook) => {
   return book
 }
 
-const findBookCover = async (param) => {
+const findBookCover = (param) => {
+  console.log(param)
   //will return a img
 }
-const renderSearch = (img, name) => {
+const renderSearch = (books) => {
   //will get the name and img and will render on screen
-  const card = `
-  <div class="cardBook">
-  <div class="headerCard">
-  <img class="bookCover" src="./assets/image.svg" alt="" />
-  <label for="selectCard">
-  <img
-  class="favoriteIcon"
-  src="./assets/heart-outline.svg"
-  alt=""
-  />
+  books.forEach((book) => {
+    let img = findBookCover(book.ISBN)
+    const card = `
+      <div class="cardBook">
+        <div class="headerCard">
+          <img class="bookCover" src="./assets/image.svg" alt="" />
+          <label for="selectCard">
+            <img class="favoriteIcon" src="./assets/heart-outline.svg" alt="" />
             <input
-            type="checkbox"
-            name="card"
-            id="selectCard"
-            aria-label="favorite"
+              type="checkbox"
+              name="card"
+              id="selectCard"
+              aria-label="favorite"
             />
-            </label>
-            </div>
-            <div class="footerCard">
-            <p class="nameOfTheBook">IT, The Thing</p>
-            </div>
-            `
+          </label>
+        </div>
+        <div class="footerCard">
+          <p class="nameOfTheBook">${book.Title}</p>
+        </div>
+      </div>
+    `
+    displayBookArea.innerHTML += card
+  })
 }
 
 const saveFavoriteBook = () => {
